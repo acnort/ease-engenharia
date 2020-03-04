772d9ac47302639563dbc8f6fcac82ce36a3e788
Ricardo Peliciolli <Ricardo@goimage.com.br>
2020-03-04 18:00:12 -0300

    fix sql

--- api/db/script.sql
+++ api/db/script.sql
@@ -4,7 +4,7 @@ CREATE TABLE `user` (
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `last_access` datetime DEFAULT NULL,
+  `lastAccess` datetime DEFAULT NULL,
   `admin` tinyint(1) DEFAULT '0',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -16,7 +16,7 @@ DROP TABLE IF EXISTS `construction`;
 CREATE TABLE `construction` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `client_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `clientName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
@@ -25,31 +25,31 @@ CREATE TABLE `construction` (
 DROP TABLE IF EXISTS `report`;
 CREATE TABLE `report` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_construction` int(11) NOT NULL,
-  `service_number` int(11) DEFAULT 0,
+  `constructionId` int(11) NOT NULL,
+  `serviceNumber` int(11) DEFAULT 0,
   `pdf` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `word` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
-  CONSTRAINT `fk_r_construction` FOREIGN KEY (`id_construction`) REFERENCES `construction` (`id`) ON DELETE CASCADE
+  CONSTRAINT `fk_r_construction` FOREIGN KEY (`constructionId`) REFERENCES `construction` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS `floor`;
 CREATE TABLE `floor` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_construction` int(11) NOT NULL,
+  `constructionId` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
-  CONSTRAINT `fk_f_construction` FOREIGN KEY (`id_construction`) REFERENCES `construction` (`id`) ON DELETE CASCADE
+  CONSTRAINT `fk_f_construction` FOREIGN KEY (`constructionId`) REFERENCES `construction` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS `item`;
 CREATE TABLE `item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_floor` int(11) NOT NULL,
+  `floorId` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `observation` text COLLATE utf8_unicode_ci,
   `rating` tinyint(4) DEFAULT '0',
@@ -57,7 +57,7 @@ CREATE TABLE `item` (
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
-  CONSTRAINT `fk_i_floor` FOREIGN KEY (`id_floor`) REFERENCES `floor` (`id`) ON DELETE CASCADE
+  CONSTRAINT `fk_i_floor` FOREIGN KEY (`floorId`) REFERENCES `floor` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- DROP TABLE IF EXISTS `construction_floor`;
@@ -79,7 +79,7 @@ CREATE PROCEDURE `user_add_or_edit` (
 )
 BEGIN
 	IF _id = 0 THEN
-		INSERT INTO user(`name`, `email`, `password`, `last_access`, `admin`, `created`, `updated`)
+		INSERT INTO user(`name`, `email`, `password`, `lastAccess`, `admin`, `created`, `updated`)
         VALUES (_name, _email, _password, _last_access, _admin, CURRENT_TIME(), CURRENT_TIME());
         
         SET _id = LAST_INSERT_ID();
@@ -89,7 +89,7 @@ BEGIN
         `name` = _name,
         `email` = _email,
         `password` = _password,
-        `last_access` = _last_access,
+        `lastAccess` = _last_access,
         `admin` = _admin,
         `updated` = CURRENT_TIME()
         WHERE `id` = _id;
